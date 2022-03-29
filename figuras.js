5ec89b56c30aaf7e7b58046d47d9f24ac7e3bde5
Jonathan <many.d.r.o.2001@hotmail.com>
2022-03-29 14:11:06 -0500

    Trabajo a medias de objetos

--- figuras.js
+++ figuras.js
@@ -1,1 +1,17 @@
-console.log("Hola!")
+let perimeter = [];
+let area = [];
+//rectangle
+let rectangle = {
+    base, height
+};
+//square
+let square = {
+    side
+};
+//triangle
+let triangle = {
+    side1, side2, side3, height
+};
+//Right triangle
+let 
+
