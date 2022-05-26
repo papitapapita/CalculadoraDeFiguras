19a1fb932f6e5676dd87f22313cc3d1bd01c1344
Jonathan <many.d.r.o.2001@hotmail.com>
2022-05-26 13:48:47 -0500

    Square completed

--- scripts/square.js
+++ scripts/square.js
@@ -50,8 +50,8 @@ class Square{
         return this.area;
     }
     calcPerimeter(){
-        this.perimeter = 2*(this.side);
-        return this.side;
+        this.perimeter = 4*(this.side);
+        return this.perimeter;
     }
     calcDiagonal(){
         this.diagonal = Math.sqrt(2)*this.side;
@@ -69,12 +69,54 @@ class Square{
     }
 }
 
-const result = document.querySelector("#result");
-console.log(value);
+const result = {
+    area: document.querySelector("#area-result"),
+    perimeter: document.querySelector("#perimeter-result"),
+    diagonal: document.querySelector("#diagonal-result"),
+    side: document.querySelector("#side-result")
+};
 
-document.querySelector("#button").onClick = function(){
-    const value = document.querySelector("#value").value;
-    let square = new Square();
-    square.setSide(value)
-    result.innerText = square.calcArea();
+const input = {
+    area: document.querySelector("#area-input"),
+    perimeter: document.querySelector("#perimeter-input"),
+    diagonal: document.querySelector("#diagonal-input"),
+    side1: document.querySelector("#side1-input"),
+    side2: document.querySelector("#side2-input"),
+    side3: document.querySelector("#side3-input")
 }
+
+let square = new Square();
+
+function calcArea(){
+    square.setSide(input.area.value);
+    result.area.innerText = square.calcArea();
+}
+
+function calcPerimeter(){
+    square.setSide(parseInt(input.perimeter.value));
+    result.perimeter.innerText = square.calcPerimeter();
+}
+
+function calcDiagonal(){
+    square.setSide(parseInt(input.diagonal.value));
+    result.diagonal.innerText = square.calcDiagonal();
+}
+
+function calcSide(){
+    if(input.side1.value != undefined && input.side1.value != ""){
+        console.log("Hi2");
+        square.setArea(parseInt(input.side1.value));
+    }else if(input.side2.value != undefined && input.side2.value != ""){
+        console.log(input.side2.value);
+        square.setPerimeter(parseInt(input.side2.value));
+    }else if(input.side3.value != undefined && input.side3.value != ""){
+        console.log(input.side3.value);
+        square.setDiagonal(parseInt(input.side3.value));
+    }
+    result.side.innerText = square.calcSide();
+    input.side1.value = input.side2.value = input.side3.value = "";
+    square.setArea(undefined);
+    square.setPerimeter(undefined);
+    square.setDiagonal(undefined);
+}
+
